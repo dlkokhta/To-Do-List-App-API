b2f6dcceb08833daee2d5cc781d3ac7d045a73e0
unknown <dl.kokhtashvili@gmail.com>
2024-11-15 18:19:15 +0400

    change swagger tags name

--- src/controllers/CompletedToDo.ts
+++ src/controllers/CompletedToDo.ts
@@ -4,24 +4,20 @@ import { initializeDb } from "../config/database";
 const CompletedToDo = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  console.log("parammssss", req.params); // Log to check the ID
   const db = await initializeDb();
 
   try {
-    // Ensure ID is an integer
     const taskId = parseInt(id, 10);
 
     if (isNaN(taskId)) {
       return res.status(400).json({ message: "Invalid task ID" });
     }
 
-    // Run the update query
     const result: any = await db.run(
       "UPDATE tasks SET completed = 1 WHERE id = ?",
       [taskId]
     );
 
-    // Check if the update affected any rows
     if (result && result.changes > 0) {
       return res.status(200).json({ message: "Task marked as completed" });
     } else {
